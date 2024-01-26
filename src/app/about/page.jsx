173660684651ea05d8c553dc21bbd3af1f05e3ac
Berkay Sergen AK <berkaysergen.ak@gmail.com>
2024-01-26 11:02:03 +0300

    12624
    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ipsa
+        blanditiis voluptate, corporis explicabo, assumenda id provident aliquam
+        ut quae tempora necessitatibus quis alias suscipit nam! Nesciunt nam
+        tenetur possimus eligendi explicabo voluptates? Quas, blanditiis fuga
+        labore, id quia at, cum magni velit nostrum accusamus quisquam possimus!
+        Suscipit, quisquam. Dignissimos aliquam dolore quidem optio rem
+        perspiciatis hic nihil, in ut laudantium repudiandae, facere explicabo
+        alias iure saepe id eligendi inventore eveniet? Quas obcaecati aliquam,
+        magnam corporis ipsa doloremque omnis perferendis incidunt dolores
+        corrupti itaque eius beatae tenetur pariatur, sequi quis totam commodi
+        labore perspiciatis aliquid, ipsum vitae.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ipsa
+        blanditiis voluptate, corporis explicabo, assumenda id provident aliquam
+        ut quae tempora necessitatibus quis alias suscipit nam! Nesciunt nam
+        tenetur possimus eligendi explicabo voluptates? Quas, blanditiis fuga
+        labore, id quia at, cum magni velit nostrum accusamus quisquam possimus!
+        Suscipit, quisquam. Dignissimos aliquam dolore quidem optio rem
+        perspiciatis hic nihil, in ut laudantium repudiandae, facere explicabo
+        alias iure saepe id eligendi inventore eveniet? Quas obcaecati aliquam,
+        magnam corporis ipsa doloremque omnis perferendis incidunt dolores
+        corrupti itaque eius beatae tenetur pariatur, sequi quis totam commodi
+        labore perspiciatis aliquid, ipsum vitae.
+      </p>
+    </div>
+  );
+}
